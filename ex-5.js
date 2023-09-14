57d5d9542e42f2f2dd78c95a7595333fb61079d0
Peeraya Natcharoenwong <Peebeary-55@gmail.com>
2023-09-14 12:41:09 +0700

    ex-5-spy

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
-// Start coding here
+let totalPrice = 27000;
+let discount25Persent = (totalPrice * 25) / 100;
+let priceAfterDiscount = discount25Persent;
+console.log(priceAfterDiscount);
